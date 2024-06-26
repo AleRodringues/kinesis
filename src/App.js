b35568860158c0990b528ff72c791ff94b10b285
Ale Rodrigues <ale.smasher@gmail.com>
2024-06-26 20:16:14 -0300

    Landing CarWidget 1.0

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <main>
-      
-      </main>
+      <ItemListContainer greeting="Bem-vindo ao Kinesis Sports Store!" />
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,28 @@
-// src/components/NavBar.js
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import CartWidget from './CartWidget';
 
 const NavBar = () => {
-    return (
-        <Navbar bg="dark" variant="dark" expand="lg">
-            <Container>
-                <Navbar.Brand href="/">Kinesis</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="/masculino">Masculino</Nav.Link>
-                        <Nav.Link href="/feminino">Feminino</Nav.Link>
-                        <Nav.Link href="/camisetas-de-futebol">Camisetas de Futebol</Nav.Link>
-                        <Nav.Link href="/tenis">Tênis</Nav.Link>
-                        <Nav.Link href="/academia">Academia</Nav.Link>
-                        <Nav.Link href="/suplementos">Suplementos</Nav.Link>
-                        <Nav.Link href="/gadgets">Gadgets</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">Kinesis</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/masculino">Masculino</Nav.Link>
+            <Nav.Link href="/feminino">Feminino</Nav.Link>
+            <Nav.Link href="/camisetas-de-futebol">Camisetas de Futebol</Nav.Link>
+            <Nav.Link href="/tenis">Tênis</Nav.Link>
+            <Nav.Link href="/academia">Academia</Nav.Link>
+            <Nav.Link href="/suplementos">Suplementos</Nav.Link>
+            <Nav.Link href="/gadgets">Gadgets</Nav.Link>
+          </Nav>
+          <CartWidget />
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default NavBar;
