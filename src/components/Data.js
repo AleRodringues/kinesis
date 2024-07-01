87fb57f586409e82b3dad4e51c217ae9d148eaea
Ale Rodrigues <ale.smasher@gmail.com>
2024-07-01 16:04:47 -0300

    add Data js

---
+++ src/components/Data.js
@@ -0,0 +1,23 @@
+export const products = [
+    {
+        id: 1,
+        title: "Camiseta Nike Dri-FIT Academy 23 - Masculina",
+        description: "Essa camiseta é tudo, menos básica.",
+        price: 119.99,
+        pictureUrl: "https://imgcentauro-a.akamaihd.net/1366x1366/M0W77201.jpg",
+    },
+    {
+        id: 2,
+        title: "TÊNIS NIKE AIR ZOOM VOMERO 16 MASCULINO",
+        description: "Sustentação com amortecimento rápido.",
+        price: 799.00,
+        pictureUrl: "https://adaptive-images.uooucdn.com.br/ik-seo/tr:w-1100,h-1594,c-at_max,pr-true,q-90/a152-ofyzta/pv/f6/00/86/90b4f05ecd5496497df2ddc0e3/tenis-nike-air-zoom-vomero-16-masculino-1-large-2.jpg",
+    },
+    {
+        id: 3,
+        title: "BCAA (10:1:1) (200G) (EM PÓ) - GROWTH SUPPLEMENTS",
+        description: " é a opção perfeita para quem deseja evolução nos treinos, como o ganho de massa muscular.",
+        price: 50.40,
+        pictureUrl: "https://www.gsuplementos.com.br/upload/growth-layout-personalizado/produto/13/bcaa-10-1-1-growth-supplements.png",
+    },
+];
